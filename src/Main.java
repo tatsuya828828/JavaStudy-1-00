3ad91b51b7292fb2cad17a960812a5b6cebf8233
tatsuya828828 <kkjy011894@gmail.com>
2020-07-09 11:48:46 +0900

    [finish]JavaStudy-1-00

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
+	static int x = 5;
+	public static double circle(int a) {
+		double c = a*a*3.14;
+		return c;
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("半径"+ x +"cmの円の面積は"+ circle(x)+ "㎠");
 	}
 }
